3d1b182b7b5465c4d9e0c4ef4c51127348d9dd65
2530111 <2530111@sw.hs.kr>
2025-07-15 14:37:31 +0900

    Update 사이트 별 사용자 만족도.py

--- pages/사이트 별 사용자 만족도.py
+++ pages/사이트 별 사용자 만족도.py
@@ -7,12 +7,10 @@ data = pd.read_csv(file path)
 
 st.header("UI/UX user interaction dataset across popular digital platforms")
 
-try:
-  if data is not None:
-    st.session_state['data'] = data
-    st.session_state['data_loaded'] = True
-    st.write("데이터셋을 성공적으로 불러왔습니다.")
-    st.write(data.head())
+st.session_state['data'] = data
+st.session_state['data_loaded'] = True
+st.write("데이터셋을 성공적으로 불러왔습니다.")
+st.write(data.head())
     with st.expander('전체 데이터 보기'):
         st.write(data)
 
