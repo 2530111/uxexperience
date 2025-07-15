1837fd834feeaf7d3e306502afe7c950891055fc
2530111 <2530111@sw.hs.kr>
2025-07-15 14:27:56 +0900

    Update 사이트 별 사용자 만족도.py

--- pages/사이트 별 사용자 만족도.py
+++ pages/사이트 별 사용자 만족도.py
@@ -2,6 +2,18 @@ import streamlit as st
 import mataplotlib.pyplot as plt
 import pandas as pd
 
+file path = 'UI_UX_Dataset.csv'
+data = pd.read_csv(file path)
+
 st.header("UI/UX user interaction dataset across popular digital platforms")
 
-file path = ''
+try:
+  if data is not None:
+    st.session_state['data'] = data
+    st.session_state['data_loaded'] = True
+    st.write("데이터셋을 성공적으로 불러왔습니다.")
+    st.write(data.head())
+    with st.expander('전체 데이터 보기'):
+        st.write(data)
+
+      
