553e68373f97f4c04406dbb8b3062519fbd40d8a
2530111 <2530111@sw.hs.kr>
2025-07-15 14:40:21 +0900

    Update 사이트 별 사용자 만족도.py

--- pages/사이트 별 사용자 만족도.py
+++ pages/사이트 별 사용자 만족도.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 file_path = 'UI_UX_Dataset.csv'
-data = pd.read_csv(file path)
+data = pd.read_csv(file_path)
 
 st.header("UI/UX user interaction dataset across popular digital platforms")
 
@@ -11,7 +11,7 @@ st.session_state['data'] = data
 st.session_state['data_loaded'] = True
 st.write("데이터셋을 성공적으로 불러왔습니다.")
 st.write(data.head())
-    with st.expander('전체 데이터 보기'):
-        st.write(data)
+with st.expander('전체 데이터 보기'):
+    st.write(data)
 
       
