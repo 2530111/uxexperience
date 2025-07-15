53ae7ed5a24758c85db5f929d0c4fc1b0b468207
2530111 <2530111@sw.hs.kr>
2025-07-15 14:49:29 +0900

    Update 사이트 별 사용자 만족도.py

--- pages/사이트 별 사용자 만족도.py
+++ pages/사이트 별 사용자 만족도.py
@@ -7,11 +7,27 @@ data = pd.read_csv(file_path)
 
 st.header("UI/UX user interaction dataset across popular digital platforms")
 
-st.session_state['data'] = data
+st.session_state['data'] = data      
 st.session_state['data_loaded'] = True
 st.write("데이터셋을 성공적으로 불러왔습니다.")
 st.write(data.head())
 with st.expander('전체 데이터 보기'):
     st.write(data)
-
       
+if st.session_state['data_loaded']:
+    data = st.session_state['data']
+    st.subheader("분석할 열을 선택해주세요.")
+    st.success(f"이 데이터는 {data.shape[0]}개의 행(가로줄), {data.shape[1]}개의 열(세로줄)로 이루어진 데이터입니다. 이중 분석할 열만 선택해주세요.")
+    if st.checkbox('모든 열 선택하기', key='select_all', value = data.columns.all()):
+        default_columns = df.columns.tolist() if 'select_all' in st.session_state and st.session_state['select_all'] else []
+    else:
+        default_columns = df.columns.tolist() if 'selected_columns' not in st.session_state else st.session_state['selected_columns']
+
+    selected_columns = st.multiselect('분석하고자 하는 열을 선택하세요:', st.session_state['df'].columns.tolist(), default=default_columns)
+    st.write(df[selected_columns].head())
+
+    st.session_state['selected_columns'] = selected_columns
+    if st.button('열 선택 완료!'):
+        st.session_state['columns_selected'] = True
+        st.success("열 선택 완료!")
+ 
